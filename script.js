a7576444079d17577e004d53047cb1c70fa8b847
Zodoig <duyguozcan@outlook.com>
2024-10-25 10:23:18 +0200

    Adjust collapsible content icon

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ function handleEvent() {
   clearPrevDetailedResults();
   const collapsibleContent = document.querySelector('.collapsible-content');
   collapsibleContent.style.display = 'none';
+  collapsibleTitle.textContent = 'Advanced Search ➡️';
 
   const apiGeoEndcode = `https://geocoding-api.open-meteo.com/v1/search?name=${inputValue}&count=1&language=en&format=json`;
 
@@ -357,6 +358,7 @@ async function handleDetailedSearch() {
   clearPrevDetailedResults();
   const collapsibleContent = document.querySelector('.collapsible-content');
   collapsibleContent.style.display = 'none';
+  collapsibleTitle.textContent = 'Advanced Search ➡️';
 
   // API call to get city coordinates
   const apiGeoEndcode = `https://geocoding-api.open-meteo.com/v1/search?name=${inputValue}&count=1&language=en&format=json`;
